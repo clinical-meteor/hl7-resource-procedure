5aac088171f2bfba56084c704feb42e83aca6e89
Abigail Watson <awatson1978@gmail.com>
2020-02-06 21:46:37 -0600

    resolving dependencies and binary build

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-procedure',
-  version: '1.7.2',
+  version: '1.7.4',
   summary: 'HL7 FHIR Resource - Procedure',
   git: 'https://github.com/clinical-meteor/hl7-resource-procedure',
   documentation: 'README.md'
